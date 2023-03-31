167410bab7aa5ccf392d2fc50f3e2d1af5a5df16
Huw Ranscombe-Smith <huwranscombesmith@gmail.com>
2023-03-31 09:27:26 +0100

    finished radian conversion challenge

--- radtodeg.py
+++ radtodeg.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+import math
+
+def rad_to_deg(rad):
+    conversion = 180 / math.pi
+    deg = rad * conversion
+    return deg
+
+def main():
+    rad = math.pi * 7 / 4
+    deg = rad_to_deg(rad)
+    print(deg)
+
+if __name__ == "__main__":
+    main()
